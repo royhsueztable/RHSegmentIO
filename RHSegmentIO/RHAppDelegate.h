4237969454e57f3c619220b14e455ce357096abf
royhsueztable <royhsu@Roys-Retina-MacBook-Pro-13.local>
2014-08-18 09:28:00 +0800

    Initial Commit

---
+++ RHSegmentIO/RHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RHAppDelegate.h
+//  RHSegmentIO
+//
+//  Created by Roy Hsu on 2014/8/18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RHSegmentIO/RHViewController.h
@@ -0,0 +1,13 @@
+//
+//  RHViewController.h
+//  RHSegmentIO
+//
+//  Created by Roy Hsu on 2014/8/18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RHViewController : UIViewController
+
+@end
